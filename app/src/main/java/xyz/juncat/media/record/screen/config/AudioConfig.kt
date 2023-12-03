ed23209cfab919cab986673ab44bd69cd0d2fd2e
jun <demon903732218@gmail.com>
2023-12-03 17:20:11 +0800

    add config data class

---
+++ app/src/main/java/xyz/juncat/media/record/screen/config/AudioConfig.kt
@@ -0,0 +1,8 @@
+package xyz.juncat.media.record.screen.config
+
+data class AudioConfig(
+    val sample: Long,
+    val channel: Int,
+    val bitrate: Long
+) {
+}
---
+++ app/src/main/java/xyz/juncat/media/record/screen/config/VideoConfig.kt
@@ -0,0 +1,20 @@
+package xyz.juncat.media.record.screen.config
+
+data class VideoConfig(
+    val width: Int,
+    val height: Int,
+    val bitrate: Long,
+    val fps: Int,
+    val iFrameInterval: Int,
+    val fpsMode: FPSMode,
+    val bitrateMode: BitrateMode
+) {
+
+    enum class FPSMode {
+        VFR, CFR
+    }
+
+    enum class BitrateMode {
+        CQ, VBR, CBR, CBR_FD
+    }
+}
